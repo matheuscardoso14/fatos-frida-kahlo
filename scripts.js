b7a09e986f662b5a22bd99d8b958f36335c777e8
Matheus <figueiredocardoso14@gmail.com>
2025-02-12 19:21:33 -0300

    Add Vite to project

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+import "./styles.scss";
+
 const formularioDeBusca = document.querySelector("form");
 const inputFato = document.querySelector(".buscar_fato__input");
 const botaoBuscar = document.getElementById("botao-buscar");
@@ -5,39 +7,41 @@ const botaoFatoAleatorio = document.getElementById("botao-aleatorio");
 const texto = document.querySelector(".sobre_frida");
 
 async function carregarFatos(url) {
-    try {
-        const fatosJSON = await fetch(url)
-        return await fatosJSON.json();
-    } catch (erro) {
-        alert("Erro ao carregar fatos. Tente novamente mais tarde.")
-        throw new Error;
-    }
+  try {
+    const fatosJSON = await fetch(url);
+    return await fatosJSON.json();
+  } catch (erro) {
+    alert("Erro ao carregar fatos. Tente novamente mais tarde.");
+    throw new Error();
+  }
 }
 
 async function buscarFato(parametro) {
-    inputFato.value = "";
-    const fatos = await carregarFatos("./fatos.json");
-    const fatoBuscado = parametro;
-    fatos.forEach((fato) => {
-        if (fatoBuscado == fato.indice) {
-            texto.style.flexDirection = "column";
-            texto.style.gap = "2rem";
-            texto.innerHTML = `<h3 class="fato-${fato.indice}_titulo fato_titulo">Fato ${fato.indice}</h3><p class="fato-${fato.indice}_texto fato_texto">${fato.texto}</p>`
-        }
-    })
+  inputFato.value = "";
+  const fatos = await carregarFatos("./fatos.json");
+  const fatoBuscado = parametro;
+  fatos.forEach((fato) => {
+    if (fatoBuscado == fato.indice) {
+      texto.style.flexDirection = "column";
+      texto.style.gap = "2rem";
+      texto.innerHTML = `<h3 class="fato-${fato.indice}_titulo fato_titulo">Fato ${fato.indice}</h3><p class="fato-${fato.indice}_texto fato_texto">${fato.texto}</p>`;
+    }
+  });
 }
 
-formularioDeBusca.addEventListener("submit", (evento) => evento.preventDefault())
+formularioDeBusca.addEventListener("submit", (evento) =>
+  evento.preventDefault()
+);
 
 inputFato.addEventListener("keypress", (evento) => {
-    if (evento.key == "Enter") {
-        buscarFato(inputFato.value);
-    }
+  if (evento.key == "Enter") {
+    buscarFato(inputFato.value);
+  }
 });
 
 botaoBuscar.addEventListener("click", () => buscarFato(inputFato.value));
 
 botaoFatoAleatorio.addEventListener("click", () => {
-    const indice = parseInt(Math.random() * 30 + 1);
-    buscarFato(indice);
-})
+  const indice = parseInt(Math.random() * 30 + 1);
+  buscarFato(indice);
+});
